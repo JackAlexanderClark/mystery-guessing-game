476e96e62ae5f89bc28ff841dde01bf0ef4dfc03
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-12-12 23:54:40 +0000

    tidy up javascript

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -16,9 +16,12 @@ $("#buttonToggle-3").click(function(){
     $("#infoToggle-3").slideToggle("slow");
 });
 
+
 function getSubmission() {
     console.log("hello");
+    getTime.textContent = "Yo wagwan";
 }
+
 
 // get time you submitted answer to display
 // source --> https://web.dev/critical-rendering-path-adding-interactivity-with-javascript/ 
@@ -26,10 +29,9 @@ var span = document.getElementsByTagName('span')[0];
 span.textContent = 'interactive';                                           // change DOM text content
 span.style.display = 'inline';                                                  // change CSSOM property
                                                             // create a new element, style it, and append it to the DOM
-var loadTime = document.createElement('div');
-loadTime.textContent = 'Congratulations! You submitted your answer at: ' + new Date() + ' Your guess was ' + getSubmission();
-loadTime.style.color = 'red';
-document.body.appendChild(loadTime);
+var getTime = document.createElement('div');
+getTime.textContent = 'Congratulations! You submitted your answer at: ' + new Date() + '.   Your guess was: ' + getSubmission();
+getTime.style.color = 'red';
+document.body.appendChild(getTime);
 
-// ???? enlarge clue inspect?
 
