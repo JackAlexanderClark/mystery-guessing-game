64ad6f7d1815be76df47abace41fe71695b71bd4
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-12-30 13:42:58 +0000

    adding images to intro rules page for context on the game. 30/12/2022

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,36 +4,68 @@
 *   firstly, the person who did the crime,
 *   secondly, the weapon used
 *   and thirdly the location.
-*/
-let correctAnswer = 4;
-let guesses = 1;
+// */
+// let correctAnswer = 4;
+// let guesses = 1;
 
-document.getElementById("guessesLeft").innerHTML = guesses;
-let playersAnswer = document.getElementById("playerAnswer").value;
+// document.getElementById("guessesLeft").innerHTML = guesses;
+// let playersAnswer = document.getElementById("playerAnswer").value;
 
-function checkAnswer() {
+// function checkAnswer() {
 
-    // handle if user's guess is empty
+//     // handle if user's guess is empty
+//     let playerNumber = $('#playerAnswer').val();
+//     if ( playerNumber == 4 ) {
+//         document.getElementById("guessesLeft").innerHTML = 0;
+//         guesses = 0;
+//         let playerNumber = $('#playerAnswer').val();
+//         document.getElementById("userChoice").innerHTML = `You chose suspect ${playerNumber} OH NO!!! this was <strong>incorrect</strong>!`;
+//         gameWon(); 
+//         return false;
+//     } else if (playersAnswer != 4 ) {
+//         document.getElementById("guessesLeft").innerHTML = 0;
+//         guesses = 0;
+//         let playerNumber = $('#playerAnswer').val();
+//         document.getElementById("userChoice").innerHTML = `You chose suspect ${playerNumber}........this was <strong>correct</strong>!`;
+//         gameLost();
+//         return false;
+//     } else {
+//         console.log("asdasd");
+//         return false;
+//     }
+let playerNumber = $('#playerAnswer').val();
+    function checkAnswer() {
+        const userGuess = Number(guessField.value);
+        if (guessCount === 1) {
+          guesses.textContent = 'Previous guesses: ';
+        }
+        guesses.textContent += `${userGuess} `;
+      
+        if (userGuess === playerNumber) {
+          lastResult.textContent = 'Congratulations! You got it right!';
+          lastResult.style.backgroundColor = 'green';
+          lowOrHi.textContent = '';
+          setGameOver();
+        } else if (guessCount === 10) {
+          lastResult.textContent = '!!!GAME OVER!!!';
+          lowOrHi.textContent = '';
+          setGameOver();
+        } else {
+          lastResult.textContent = 'Wrong!';
+          lastResult.style.backgroundColor = 'red';
+          if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Last guess was too low!';
+          } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Last guess was too high!';
+          }
+        }
+      
+        guessCount++;
+        guessField.value = '';
+        guessField.focus();
+      }
 
 
-    if ( playersAnswer != 4 ) {
-        document.getElementById("guessesLeft").innerHTML = 0;
-        guesses = 0;
-        let playerNumber = $('#playerAnswer').val();
-        document.getElementById("userChoice").innerHTML = `You chose suspect ${playerNumber} xxx Well done this was correct!`;
-        gameWon(); 
-        alert("Empty answer try again.");
-        return false;
-    }
-
-    if (playersAnswer == 4 ) {
-        document.getElementById("guessesLeft").innerHTML = 0;
-        guesses = 0;
-        let playerNumber = $('#playerAnswer').val();
-        document.getElementById("userChoice").innerHTML = `You chose suspect ${playerNumber}........s was incorrect!`;
-        gameLost();
-        return false;
-    }
 
     // if (playersAnswer === "") {
     //     alert("Please enter a guess.");
@@ -41,11 +73,11 @@ function checkAnswer() {
     // } 
 
     // stop form being submitted CHAT GPT
-    return false;
+//     return false;
 
-}
+// }
 
-// prevent multiple guesses using jquery .one() method
+// // prevent multiple guesses using jquery .one() method
 $('#answerButton').one('click', function() {
     $(this).css({'cursor':'not-allowed', 'color': 'white', 'background-color': 'red'}).attr('disabled','disabled');
 });
