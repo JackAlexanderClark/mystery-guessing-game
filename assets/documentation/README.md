465752e46d7bc56c2dbe528401efeee43ee40dfa
JackAlexanderClark <jackalexanderclark@protonmail.com>
2023-01-15 18:09:58 +0000

    added new html page hints.html. 15/01/2023

--- assets/documentation/README.md
+++ assets/documentation/README.md
@@ -7,7 +7,7 @@
 
 ## Website Device Mockup
 
-# **I) Project Goals
+# ##I) Project Goals
 
 1. For this project I decided upon a guessing game as a foundation, that would allow user inputs and ability to interact with different areas of the project. However, I wanted to expand upon this with added complexity and a more in-depth story. The game is inspired by the board game Cluedo and to get an aesthetically pleasing layout I commision some artwork to be produced for the characters in the game. I wanted the players to look like a playing card to brinbg that trading card game/board game to life, as seen below; ![image](https://user-images.githubusercontent.com/97599832/212542303-b60b7be1-4f68-4a37-becc-c9d197ef0983.png)
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,12 @@
-/*
-*   script.js contains scripting for effects and interactions outside of the game loop
-*/
+// tippy js module tooltip 
+tippy('#buttonSubmit', {
+    content: "Have you chosen a correct username and password?",
+});
 
-const draggables = document.querySelectorAll('.draggable')
-const containers = document.querySelectorAll('.container')
-
-draggables.forEach(draggable => {
-  draggable.addEventListener('dragstart', () => {
-    draggable.classList.add('dragging')
-  })
-
-  draggable.addEventListener('dragend', () => {
-    draggable.classList.remove('dragging')
-  })
-})
-
-containers.forEach(container => {
-  container.addEventListener('dragover', e => {
-    e.preventDefault()
-    const afterElement = getDragAfterElement(container, e.clientY)
-    const draggable = document.querySelector('.dragging')
-    if (afterElement == null) {
-      container.appendChild(draggable)
-    } else {
-      container.insertBefore(draggable, afterElement)
-    }
-  })
-})
-
-function getDragAfterElement(container, y) {
-  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
-
-  return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
-    if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
-    } else {
-      return closest
-    }
-  }, { offset: Number.NEGATIVE_INFINITY }).element
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+// practice tippy js - https://atomiks.github.io/tippyjs/
+tippy('#myButton', {
+  content: "I'm a Tippy tooltip!",
+});
 
 
 
