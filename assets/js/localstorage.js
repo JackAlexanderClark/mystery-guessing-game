0ebf44889587121f1f5328c7fd7528f5b37ddef1
JackAlexanderClark <jackalexanderclark@protonmail.com>
2023-01-27 14:13:15 +0000

    resized images to look better on broswer and mobile. 27/01/2023

--- assets/js/localstorage.js
+++ assets/js/localstorage.js
@@ -43,14 +43,25 @@ inputButton.onclick = function() {
     }
 
 };
+// credit to:"https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript#:~:text=Use%20new%20Date()%20to,the%20current%20date%20and%20time.&text=This%20will%20give%20you%20today's,to%20whatever%20format%20you%20wish."
+// try get date to append to session local information
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+
+let sessionNumber = 0;
 
 // loop through contents of localstorage
 for (let i = 0; i < localStorage.length; i++ ) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
-
+    sessionNumber++;
     // template literal to populate html
-    outputDiv.innerHTML += `${key}: ${value}<br>`;
+    outputDiv.innerHTML += `${key}: ${value}<br> Date: ${today} Session: ${sessionNumber}`;
 
 }
 
@@ -58,27 +69,6 @@ for (let i = 0; i < localStorage.length; i++ ) {
 // let retrievedDump = localStorage.getItem("x");
 // document.getElementById("dumpResult").innerHTML = retrievedDump;
 
-
-
-// enter password to submit your answer
-function checkPassword() {
-    console.log("test");
-}
-
-// take name from localstorage to display in winning message if game won
-function getUsername() {
-
-    var getName = localStorage.getItem(key, value);
-    gameWonDisplayUsername(getName);
-
-}
-
-// if user wants to play again, clear cache
-function resetLocalStorage() {
-
-    localStorage.clear(key);
-
-}
 
 
 
